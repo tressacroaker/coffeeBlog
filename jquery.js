494b6a011715a8c910f8ac4237897ff68bcf4b80
tressacroaker <tressacroaker36@gmail.com>
2016-10-17 05:32:15 -0600

    basic layout

--- jquery.js
+++ jquery.js
@@ -8,11 +8,50 @@ $(document).ready(function() {
       $("#cabin").fadeTo("slow", 0.4, function() {});
   });
 
-  // $("#music").hover(function() {
-  //     $("#music").fadeTo("slow", 0.4, function() {});
-  // });
+  $("#sailing").hover(function() {
+      $("#sailing").fadeTo("slow", 0.4, function() {});
+    });
 
+  $("#athens").hover(function() {
+      $("#athens").fadeTo("slow", 0.4, function() {});
+    });
 
+  $("#grotto").hover(function() {
+      $("#grotto").fadeTo("slow", 0.4, function() {});
+      });
 
+  $("#greece").hover(function() {
+      $("#greece").fadeTo("slow", 0.4, function() {});
+      });
+
+  $("#rooftop").hover(function() {
+      $("#rooftop").fadeTo("slow", 0.4, function() {});
+  });
+
+  $(".quoteBox").hover(function() {
+      $(".quoteBox").css("color", "#AFD4DC");
+  });
+
+  $("#journal").hover(function() {
+      $("#journal").css("text-decoration", "underline");
+  });
+  $("#bio").hover(function() {
+      $("#bio").css("text-decoration", "underline");
+  });
+  $("#travels").hover(function() {
+      $("#travels").css("text-decoration", "underline");
+  });
+  $("#photographs").hover(function() {
+      $("#photographs").css("text-decoration", "underline");
+  });
+
+//   $("#photographs").hover(function() {
+//     $(".photodropdown").toggle();
+// });
+//
+//
+// $("#travels").on("click", function() {
+//     $(".traveldropdown").slideToggle();
+// });
 
 });
